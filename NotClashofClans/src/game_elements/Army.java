99d443179d424ee48ef72d632433bdd916269211
Helena Coutinho <helena@Helenas-MacBook-Air-2.local>
2026-03-08 14:27:29 -0400

    small fix to army, unit addition, getters

--- NotClashofClans/src/game_elements/Army.java
+++ NotClashofClans/src/game_elements/Army.java
@@ -3,10 +3,36 @@ package game_elements;
 import java.util.ArrayList;
 
 public class Army {
-    private int size;
-    ArrayList<ArmyUnit> armyUnits = new ArrayList<>();
+    private ArrayList<ArmyUnit> armyUnits = new ArrayList<>();
 
+    /*
+     * Returns the number of units in the army
+     *
+     * @return the size of the army
+     */
     public int getSize() {
-        return size;
+        return armyUnits.size();
+    }
+
+    /*
+     * Returns the list of all units in the army
+     *
+     * @return the list army units
+     */
+    public ArrayList<ArmyUnit> getArmyUnits() {
+        return armyUnits;
+    }
+
+    /*
+    * Adds unit to the army
+    *   
+    * @param unit the unit to be added to army
+    */
+    public void addUnit(ArmyUnit unit){
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null.");
+        }
+
+        armyUnits.add(unit);
     }
 }
