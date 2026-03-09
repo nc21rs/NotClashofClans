c13c3ffbb37a48d84b17d6d102e342ac009e310b
Helena Coutinho <helena@Helenas-MacBook-Air-2.local>
2026-03-08 23:40:50 -0400

    class doc for army generator

--- NotClashofClans/src/game_engine/ArmyGenerator.java
+++ NotClashofClans/src/game_engine/ArmyGenerator.java
@@ -4,7 +4,14 @@ import game_elements.Army;
 
 import java.util.Random;
 
+/**
+ * This class is responsible for generating armies in the game that attack villages.
+ */
 public class ArmyGenerator {
-    private Random random;
-    public Army createArmy(){return new Army();}
+    private Random dice;
+
+    public Army createArmy(){
+        return new Army();
+    }
+
 }
